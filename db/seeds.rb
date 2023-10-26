152291893240083be023dc0262588f30c002d0cb
COURTOIS <courtois.a@hotmail.com>
2023-10-26 23:37:55 +0200

    Ajout User, City, Gossip, Tag et seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,8 +16,43 @@ Tag.destroy_all
 City.destroy_all
 
 
-5.times do |index|
+10.times do |index|
   City.create!(
-    city_name: Faker::Address.city
+    name: Faker::Address.city,
+    zip_code: Faker::Address.zip
    )
+end
+
+10.times do |index|
+  User.create!(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    description: Faker::Lorem.sentence(word_count: 5),
+    email: Faker::Internet.email,
+    age: Faker::Number.within(range: 18..65),
+    city: City.all.sample
+  )
+end
+
+20.times do |index|
+  Gossip.create!(
+    title: Faker::Book.title,
+    content: Faker::Quote.matz,
+    user: User.all.sample
+  )
+end
+
+10.times do |index|
+  Tag.create!(
+    title: Faker::Book.genre 
+  )
+end
+
+Gossip.all.each do |gossip|
+  2.times do |index|
+    GossipTag.create!(
+      gossip: gossip,
+      tag: Tag.all.sample
+    )
+  end
 end
